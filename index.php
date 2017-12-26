1be0227778b97a1fb9218b83b1ca56099ba9bff9
Julio Rezende <juliorezendee@gmail.com>
2017-12-26 21:01:28 -0200

    adm categ

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Jcode\Page;
 use \Jcode\PageAdmin;
+use \Jcode\Model\User;
+use \Jcode\Model\Category;
+
 
 $app = new Slim();
 
@@ -19,12 +22,14 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
     
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
 });
 
-$app->get('/admin/login', function(){
+$app->get('/admin/login', function() {
 
 	$page = new PageAdmin([
 		"header"=>false,
@@ -34,6 +39,160 @@ $app->get('/admin/login', function(){
 	$page->setTpl("login");
 
 	});
+
+$app->post('/admin/login', function() {
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+
+});
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+});
+
+$app->get("/admin/users", function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users");
+
+});
+
+$app->get("/admin/users/create", function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");
+
+});
+
+$app->get("/admin/users/:idusers", function($iduser){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update");
+
+});
+
+$app->post("/admin/users/create", function() {
+	
+	User::verifyLogin();
+
+
+});
+
+$app->post("/admin/users/:iduser", function($iduser) {
+	
+	User::verifyLogin();
+
+
+});
+
+$app->delete("/admin/users/:iduser", function($iduser) {
+	
+	User::verifyLogin();
+
+
+});
+
+$app->get("/admin/categories", function(){
+
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+		'categories'=>$categories
+	]);
+
+});
+
+$app->get("/admin/categories/create", function(){
+
+	User::verifyLogin();
+	
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function(){
+
+	User::verifyLogin();
+	
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit;
+
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+	
+	User::verifyLogin();
+	
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header('Location: /admin/categories');
+	exit;
+});
+
+$app->get("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", [
+		'category'=>$category->getValues()
+	]);
+
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit;
+
+});
+
 
 $app->run();
 
